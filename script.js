086efe3fc1eee35d309d869cb319081c1f1552b7
nnicoless <nicole.bugdanovicz.luz@escola.pr.gov.br>
2025-10-19 15:05:30 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -75,17 +75,67 @@ const infosProdutos = {
 
    chainfusao: {
     nome: "Infusões De Frutas",
-    descricao: "Bebida quente e aromática, feita com misturas de frutas naturais. Sabores: Morango, limão, laranja e frutas vermelhas.",
+    descricao: "Bebida quente e aromática, feita com misturas de frutas naturais.<br><br> <strong> Sabores: </strong> <br> Morango; <br> Limão; <br> Laranja; <br> Frutas vermelhas.",
   },
 
    chagelado: {
     nome: "Chá Gelado Especial",
-    descricao: "Refrescante e leve, servido com gelo. Sabores: limão, pêssego, maracujá, morango e laranja.",
+    descricao: "Refrescante e leve, servido com gelo.<br><br> <strong> Sabores:</strong> <br> Limão; <br> Pêssego; <br> Maracujá; <br> Morango; <br> Laranja.",
   },
 
     chacomleite: {
     nome: "Chá Com Leite",
     descricao: "Bebida feita com chá mate quente misturado ao leite, resultando em sabor suave e cremoso.",
+  },
+
+ paodequeijo: {
+    nome: "Pão De Queijo",
+    descricao: "Tradicional quitute mineiro, macio e de sabor irresistível.",
+  },
+
+   croissantsimples: {
+    nome: "Croissant Simples",
+    descricao: "Massa folhada leve e amanteigada, assada até dourar.",
+  },
+
+   croissantrecheado: {
+    nome: "Croissant Recheado",
+    descricao: "Croissant crocante e amanteigado com recheio de sua escolha.<br><br> <strong>OPÇÕES:<br><br> Tradicionais:</strong> <br>Presunto e Queijo; <br>Queijo e Salame; <br>Frango com Catupiry; <br> Queijo e Tomate. <br><strong>Especiais: </strong> <br>Queijo Brie com Presunto cru e Geléia de Damasco; <br> Caprese; <br> Patê de Ricota e Alho-Poró.",
+  },
+
+   empada: {
+    nome: "Empada",
+    descricao: "Massa leve e amanteigada com recheio cremoso. <br> <br> <strong> OPÇÕES:</strong> <br> Frango com Requeijão; <br> Carne Seca com Catupiry; <br> Palmito; <br> Queijo com Alho-Poró.",
+  },
+
+   coxinha: {
+    nome: "Coxinha",
+    descricao: "Tradicional salgado brasileiro, com massa macia por dentro e frito até ficar crocante por fora. <br> <br> <strong> OPÇÕES:</strong> <br> Tradicional: Frango (Com ou Sem Catupiry);<br> Massa de Mandioca com recheio de Costela."
+  },
+
+   esfihaaberta: {
+    nome: "Esfiha Aberta",
+    descricao: "Massa leve e assada, moldada em formato aberto e servida quente. <br> <br> <strong> OPÇÕES: </strong> <br> Carne; <br> Queijo; <br> Frango; <br> Calabresa.",
+  },
+
+   esfihafechada: {
+    nome: "Esfiha Fechada",
+    descricao: "Massa macia e dourada, moldada em formato triangular e assada no forno. <br> <br> <strong>OPÇÕES: </strong> <br> Carne; <br> Frango; <br> Queijo e Presunto;",
+  },
+
+   tortasalgada: {
+    nome: "Torta Salgada",
+    descricao: "Massa leve e dourada com recheio cremoso de frango e requeijão.",
+  },
+
+   sanduiche: {
+    nome: "Sanduíche Natural",
+    descricao: "Pão leve e fresco com recheio equilibrado, ideal para uma refeição rápida e saudável. <br> <br> <strong>OPÇÕES:</strong> <br> Frango; <br> Peito de peru; <br> Atum; <br> Vegetariano <br> <strong>OBS: todos acompanham alface, tomate e queijo mussarela.</strong>",
+  },
+
+   tapioca: {
+    nome: "Tapioca",
+    descricao: "Massa leve feita de goma de mandioca, grelhada até ficar macia e levemente crocante. <br> <br> <strong>OPÇÕES: <br> Tradicionais:</strong> <br> Queijo; <br> Queijo e Presunto; <br> Frango com Requeijão; <br> Ovo com Queijo; <br> Atum com Maionese. <br> <strong>Especiais;</strong> <br> Tomate Seco com Manjericão; <br> Queijo Coalho com Mel; <br> Carne Seca com Requeijão. ",
   },
 };
 
@@ -97,7 +147,7 @@ document.addEventListener('click', (e) => {
   const info = infosProdutos[alt];
   if (info) {
     cardNome.textContent = info.nome;
-    cardDesc.textContent = info.descricao;
+    cardDesc.innerHTML = info.descricao;
     card.style.display = 'flex';
   }
 });
